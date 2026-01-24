8e14991d595748d789e46d4bbbb586f3812dcbaf
Theodoros Traitsis <thodoristraitsis@hotmail.gr>
2026-01-24 23:57:47 +0200

    feat: invalid login & contact test cases

--- tests/contacts.spec.ts
+++ tests/contacts.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import testData from '../test-data/testData.json';
 import { ContactListPage } from '../pages/ContactListPage';
@@ -17,4 +17,18 @@ test('Add a new contact and verify that the contact is displayed.', async ({ pag
   await addContactPage.addContact(testData.validUser.firstName, testData.validUser.lastName, testData.validUser.birthdate, testData.validUser.email, testData.validUser.phone, testData.validUser.streetAddress1, testData.validUser.streetAddress2, testData.validUser.city, testData.validUser.stateOfProvince, testData.validUser.postalCode, testData.validUser.country);
   await contactListPage.expectAddedContactToBeVisible(testData.validUser.firstName, testData.validUser.lastName, testData.validUser.birthdate, testData.validUser.email, testData.validUser.phone, testData.validUser.streetAddress1, testData.validUser.streetAddress2, testData.validUser.city, testData.validUser.stateOfProvince, testData.validUser.postalCode, testData.validUser.country);
 
+});
+
+test('Verify error message for missing requierd fields.', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const contactListPage = new ContactListPage(page);
+  const addContactPage = new AddContactPage(page);
+
+  await page.goto('');
+  await loginPage.login(testData.validUser.email, testData.validUser.password);
+  await contactListPage.expectPageToBeVisible();
+  await contactListPage.addContactButton.click();
+  await addContactPage.expectPageToBeVisible();
+  await addContactPage.submitButton.click();
+  await expect(addContactPage.errorMessage).toBeVisible();
 });
---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { generateRandomUser } from '../utils/helperFunctions';
+
+test('Verify error messgae for incorrect username or password.', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const user = generateRandomUser();
+
+  await page.goto('');
+  await loginPage.emailInput.fill(user.email);
+  await loginPage.passwordInput.fill(user.password);
+  await loginPage.submitButton.click();
+  await expect(loginPage.errorMessage).toBeVisible();
+
+});
